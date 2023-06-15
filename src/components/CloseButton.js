c4c8efeb9e3f313b64f03277ffa53d1b794ec7e6
AleksNau <workciti@yandex.ru>
2023-06-15 21:45:07 +0300

    сделать профайл

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -3,8 +3,6 @@ import ProfilePopup from "./ProfilePopup";
 
 
 const CloseButton = ({ active,setActive }) => {
-    const [show, setShow] = useState(true);
-    const handleClose = () => setShow(false);
     return (
         <div>
             <button type="button"  className="popup__close-button" onClick={()=>setActive(false)}></button>
