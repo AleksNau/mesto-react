cad9a9117393433bb19d5d34ab6d0d79d967cd7f
AleksNau <workciti@yandex.ru>
2023-06-16 13:02:11 +0300

    rename

--- src/components/main.js
+++ src/components/main.js
@@ -8,27 +8,27 @@ import AddPopup from "./AddPopup";
 
 
 const Main = () => {
-    const [modalActive, setModalActive] = useState(false);
-    const [modalActiveAvatar, setModalActiveAvatar] = useState(false);
-    const [modalActiveAdd, setModalActiveAdd] = useState(false);
+    const [isEditProfilePopupOpen, setModalActive] = useState(false);
+    const [isEditAvatarPopupOpen, setModalActiveAvatar] = useState(false);
+    const [isAddPlacePopupOpen, setModalActiveAdd] = useState(false);
         return (
             <main className="main">
             <section className="profile">
                 <div className="profile__info-conteiner">
                     <img src="#" alt="Жак Кусто" className="profile__avatar"></img>
-                    <AvatarPopupButton active={modalActiveAvatar} setActive={setModalActiveAvatar}/>
+                    <AvatarPopupButton active={isEditAvatarPopupOpen} setActive={setModalActiveAvatar}/>
                     <h1 className="profile__name">Жак-Ив Кусто</h1>
-                    <ProfilePopupButton active={modalActive} setActive={setModalActive}/>
+                    <ProfilePopupButton active={isEditProfilePopupOpen} setActive={setModalActive}/>
                     <p className="profile__info">Исследователь океана</p>
                 </div>
-                <AddPopupButton active={modalActiveAdd} setActive={setModalActiveAdd}/>
+                <AddPopupButton active={isAddPlacePopupOpen} setActive={setModalActiveAdd}/>
             </section>
             <ul className="elements">
 
             </ul>
-                <ProfilePopup active={modalActive} setActive={setModalActive}/>
-                <AvatarPopup active={modalActiveAvatar} setActive={setModalActiveAvatar}/>
-                <AddPopup active={modalActiveAdd} setActive={setModalActiveAdd}/>
+                <ProfilePopup active={isEditProfilePopupOpen} setActive={setModalActive}/>
+                <AvatarPopup active={isEditAvatarPopupOpen} setActive={setModalActiveAvatar}/>
+                <AddPopup active={isAddPlacePopupOpen} setActive={setModalActiveAdd}/>
         </main>
         )    
 }
