cba98c7ccc1bcfbc4d4540be89e3b08cc793606b
AleksNau <workciti@yandex.ru>
2023-06-18 17:26:14 +0300

    сделать

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,8 +4,8 @@ export default function ImagePopup({isOpen, setActive, image,name}) {
 
     return (
         <div className={isOpen ? "popup popup_opened" :"popup"} onClick={() => setActive(false)}>
-            <div className="popup__conteiner-zoom">
-                <button type="button" className="popup__close-button"></button>
+            <div className="popup__conteiner-zoom" onClick={event => event.stopPropagation()}>
+                <button type="button" className="popup__close-button" onClick={() => setActive(false)}></button>
                 <img src={image} alt="" className="popup__image" ></img>
                 <p className="popup__place-name">{name}</p>
             </div>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import CloseButton from "./CloseButton";
 
 export default function PopupWithForm({isOpen, setActive, form, popupName}) {
     return (
         <div className={isOpen ? `popup popup_opened ${popupName}` :`popup ${popupName}`} onClick={() => setActive(false)}>
-            <CloseButton setActive={setActive}/>
             <div className="popup__conteiner" onClick={event => event.stopPropagation()}>
+                <button type="button" className="popup__close-button" onClick={() => setActive(false)}></button>
                 {form}
             </div>
         </div>
