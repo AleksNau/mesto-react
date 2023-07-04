a440245ea271639096db7306e7e59afae2c4005b
AleksNau <workciti@yandex.ru>
2023-07-04 07:42:15 +0300

    исправить импорты

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import "../index.css";
 import Footer from "./Footer.js";
 import Header from "./Header.js";
