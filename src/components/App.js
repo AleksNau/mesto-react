99166608199e1aee72a4d4f6f826da77952f68e7
AleksNau <workciti@yandex.ru>
2023-07-04 07:38:40 +0300

    исправить мелочи

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ const App = () => {
     handleCardClick({});
   }
 
-  const isOpen = isPopupAvatar || isEditProfilePopupOpen || isAddPlacePopupOpen || selectedCard.link
+  const isOpen = isPopupAvatar || isEditProfilePopupOpen || isAddPlacePopupOpen || selectedCard.link;
 
   useEffect(() => {
     function closeByEscape(evt) {
